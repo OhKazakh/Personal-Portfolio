31b35b7419235bb7d007dc665486f36aa5157656
Danial <danbaluanov@bk.ru>
2024-03-10 17:25:17 +0500

    new

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 function opentab(tabname, event) {
-      for (tablink of tablinks) {
-           tablink.classList.remove("active-link");
-     }
-      for (tabcontent of tabcontents) {
-         tabcontent.classList.remove("active-tab");
-       }
-     event.currentTarget.classList.add("active-link");
-     document.getElementById(tabname).classList.add("active-tab");
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
 }
 var sidemenu = document.getElementById("sidemenu");
 function openmenu(){
@@ -27,7 +27,7 @@ const texts = [
 	"Future Canadian International School Student",
 ];
 const morphTime = 1;
-const cooldownTime = 0.25;
+const cooldownTime = 1.25;
 let textIndex = texts.length - 1;
 let time = new Date();
 let morph = 0;
